6a739042668515cf20e8f201192e3ac7c522976a
thiago rodrigues <thiagorodrigues15@hotmail.com.br>
2022-08-29 19:55:45 -0300

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `my-gatsby-site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
