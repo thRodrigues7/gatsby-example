3298d47b7c2ff5e551f2165ef66bcd33cfccb20a
thiago rodrigues <thiagorodrigues15@hotmail.com.br>
2022-08-30 23:38:58 -0300

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `my-gatsby-site`,
+    title: "My first gatsby site",
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
